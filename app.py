3ec5b3610df5a621506bd5415d4a30c248f60981
kinteiro <davidmquinteiro@gmail.com>
2024-01-20 13:12:34 +0100

    Changed home text

--- app.py
+++ app.py
@@ -11,14 +11,16 @@ from ml_app import run_ml_app
 def home_page():
 	# Título de la APP con una caja azul  y el texto en blanco centrado en la pantalla con el texto: Diabetes Predictor
 
-	st.title('App para la detección temprana de Diabetes')
-	st.text('Esta aplicación permite predecir la diabetes en pacientes')
-	st.markdown('## fuentes de información')
-	st.code("""- https://www.kaggle.com/uciml/pima-indians-diabetes-database 
-	          \n- https://www.kaggle.com/uciml/pima-indians-diabetes-database""", language='markdown')
+	st.title('App para la deteccion temprana de DM')
+	st.text('Dataset que contiene señales y sintomas que pueden indicar diabetes o posibilidad de diabetes.')
+	st.markdown('## fuentes de datos')
+	st.code("""
+			https://archive.ics.uci.edu/ml/datasets/Early+stage+diabetes+risk+prediction+dat""", 
+			language='markdown')
 	st.markdown('## Contenido de la aplicación')
-	st.code("""- https://www.kaggle.com/uciml/pima-indians-diabetes-database 
-	          \n- https://www.kaggle.com/uciml/pima-indians-diabetes-database""", language='markdown')
+	st.code("""- EDA Section: Análisis exploratorio de los datos
+			\n- ML Section: Predicción de Diabetes basada en ML (Machine Learning)""", 
+language='markdown')
 
 
 
